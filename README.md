f080046e4f7a1c7020d4823c1bc7ee1ac3a9ed77
CaioSrf <caiosrfreitas@gmail.com>
2024-04-24 22:24:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Meu Portfolio
+
+Bem-vindo ao meu portfólio! Este é um projeto desenvolvido utilizando Vite, Tailwind CSS, React Icons, npm e ESLint. 
+
+## Como começar Siga estas instruções para obter uma cópia deste projeto e executá-lo localmente para desenvolvimento e testes. 
+ 
+### Pré-requisitos Certifique-se de ter o Node.js instalado em sua máquina antes de prosseguir.
+
+### Instalação 
+
+1. Clone este repositório em sua máquina local: 
+```sh git clone https://github.com/CaioSrf/Portfolio.git 
+cd Portfolio 
+
+### Dependencias 
+npm install 
+npm install -D tailwindcss 
+npm install react-icons --save 
+npm run dev 
+
+### Estilo de código Este projeto utiliza ESLint para garantir um estilo de código consistente. Certifique-se de que seu editor de código está configurado para suportar ESLint.
---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Sou um desenvolvedor full stack entusiasmado, com um talento natural para criar aplicações web poderosas e dimensionáveis. Ao longo de 2 anos de prática dedicada, aprimorei minha proficiência em tecnologias front-end, incluindo React e Next.js, além de tecnologias back-end como Node.js, Python, MySQL e Firebase.
+
+Meu objetivo é canalizar essa experiência para conceber soluções inovadoras que não apenas impulsionem o crescimento dos negócios, mas também proporcionem experiências excepcionais aos usuários. Estou comprometido em liderar a criação de plataformas que conectem e inspirem pessoas em todo o mundo, elevando a maneira como interagimos e compartilhamos nossas vidas online.`;
+
+export const ABOUT_TEXT = `Eu prospero em ambientes colaborativos e gosto de resolver problemas complexos para entregar soluções de alta qualidade. Fora da programação, gosto de permanecer ativo, explorar novas tecnologias e contribuir para projetos de código aberto.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Atualmente",
+    role: "Estagiário",
+    company: "SpeedMais+.",
+    description: `Suporte a usuários e atualização de dados no sistema`,
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Mc Donalds",
+    image: project1,
+    description:
+      "Replica da pagina do MC DONALDS, projeto realizado para o VAI NA WEB ",
+    technologies: ["HTML", "CSS", "React", "Node.js"],
+    githubLink: "https://github.com/CaioSrf/McDonalds"
+  },
+  {
+    title: "SwapStop",
+    image: project3,
+    description:
+      "Aplicativo para celular voltado para compra e venda de produtos",
+    technologies: ["React.Js", "Node.js", "Firebase"],
+    githubLink: "https://github.com/CaioSrf/SwapStop"
+  },
+];
+
+export const CONTACT = {
+  address: "Olinda, PE ",
+  phoneNo: "+55 81  986965174",
+  email: "caiosrfreitas@gmail.com",
+};
